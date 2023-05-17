52dc4f1c21815a4a16fa7087aeff290d060ef939
te-mi <te-mi@Paulo>
2023-05-17 02:24:38 -0300

    PH2308

---
+++ OneDrive/Documentos/NetBeansProjects/mavenproject5/src/main/java/com/mycompany/mavenproject5/Mavenproject5.java
@@ -0,0 +1,15 @@
+questao 2
+
+import java.util.Stack;
+
+public class Main{
+
+    public static void main(String[] args) {
+        Action action = new Action();
+        action.value = 10;
+
+        Stack<Action> stack = new Stack<>();
+        stack.push(value);
+
+    }
+}
